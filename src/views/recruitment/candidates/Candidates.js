e37538a336b06b5fc6fa2ba803f631fbcbf20d44
Sel <mcriselda.dc@yahoo.com>
2024-09-20 16:09:30 -0400

    initial commit

---
+++ src/views/recruitment/candidates/Candidates.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Candidates = () => {
+  return (
+    <div>
+      <h1>Candidates Page</h1>
+      <p>This is the Candidates page where you can manage job postings.</p>
+      {/* Add more content and logic as needed */}
+    </div>
+  );
+};
+
+export default Candidates;
